6af471ea30ac4549f232a78a9e5e3a21ae1fdc46
Akshay Bhardwaj <akshay.bhardwaj@avizva.com>
2025-06-29 19:18:19 +0530

    Changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react";
 export default defineConfig({
+  base: "/roster-app/",
   plugins: [react()],
   server: {
     port: 5173,
