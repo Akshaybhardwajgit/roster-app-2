dd9db403230c1d20c6f5893cdb423465a2635fc3
Akshay Bhardwaj <akshay.bhardwaj@avizva.com>
2025-06-29 19:48:00 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { Suspense, lazy } from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 const ProvidersCalendar = lazy(() => import('./components/ProvidersCalendar'));
 const ViewCalendar = lazy(() => import('./components/ViewCalendar'));
@@ -9,14 +9,14 @@ const ViewCalendar = lazy(() => import('./components/ViewCalendar'));
 export default function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <Suspense fallback={<div className="p-6">Loading…</div>}>
           <Routes>
             <Route path="/" element={<ProvidersCalendar />} />
             <Route path="/calendar/:id" element={<ViewCalendar />} />
           </Routes>
         </Suspense>
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   );
 }
