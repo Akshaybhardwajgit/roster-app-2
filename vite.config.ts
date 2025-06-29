741454d0091516bf572e5c05eb8467db1c95f5a8
Akshay Bhardwaj <akshay.bhardwaj@avizva.com>
2025-06-29 19:26:02 +0530

    removed route

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
-  base: "/roster-app/",
   plugins: [react()],
   server: {
     port: 5173,
